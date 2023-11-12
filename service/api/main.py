f37e95d845fb6ed574e893b1ea89bd3ac7074562
ikarabulut <i.karabulut93@gmail.com>
2023-11-11 21:45:06 -0600

    add GET to get model info

--- service/api/main.py
+++ service/api/main.py
@@ -3,5 +3,13 @@ from joblib import load
 
 clf = load("model.joblib")
 
-
 app = FastAPI()
+
+
+@app.get("/model_information")
+def model_information():
+    return clf.get_params()
+
+
+if __name__ == "__main__":
+    print("running... ")
